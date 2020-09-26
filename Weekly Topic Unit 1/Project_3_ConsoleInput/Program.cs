b2d2eb270d686b96cb87ccd75c5178fe8ca02838
Cookies4Lunch <71670065+Cookies4Lunch@users.noreply.github.com>
2020-09-25 21:45:04 -0500

    Conclusion of the Weekly Topic Unit 1

---
+++ Weekly Topic Unit 1/Project_3_ConsoleInput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project_3_ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring and assigning strings
+            string firstname = "John";
+            string lastname = "Doe";
+
+            Console.WriteLine("Name: " + firstname + " " + lastname);
+
+            Console.WriteLine("Please enter a new first name:");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("Please enter a new last name:");
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("New name: " + firstname + " " + lastname);
+
+            Console.Write("Press any key to continue");
+            Console.ReadKey();
+
+        }
+    }
+}
