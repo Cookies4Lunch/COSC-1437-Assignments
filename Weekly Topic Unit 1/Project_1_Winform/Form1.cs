e7cd2b3e36da8b8f61211845d90cc23eadc5fdbd
Cookies4Lunch <71670065+Cookies4Lunch@users.noreply.github.com>
2020-09-25 20:59:33 -0500

    Conclusion of the Weekly Topic Unit 1

---
+++ Weekly Topic Unit 1/Project_1_Winform/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Spencer M Johnson
+ */
+
+namespace Project_1_Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMessageBoxPopup_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "This is a message box", 
+                "This is the title bar",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+        }
+    }
+}
---
+++ Weekly Topic Unit 1/Project_2_VB_Console/Program.vb
@@ -0,0 +1,17 @@
+Imports System
+
+'Written by Mark Reynolds
+'Recreated by Spencer M Johnson
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Project #2")
+        Console.WriteLine("by Spencer Johnson")
+        Console.WriteLine()
+
+        Console.WriteLine("Press any key to continue")
+
+        Console.ReadKey()
+
+    End Sub
+End Module
