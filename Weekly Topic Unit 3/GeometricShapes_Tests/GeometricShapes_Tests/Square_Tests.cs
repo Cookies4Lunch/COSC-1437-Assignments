2c2a8fd14dd3bd58bb5e5230d14f1e795eee8813
Cookies4Lunch <71670065+Cookies4Lunch@users.noreply.github.com>
2020-10-09 16:09:33 -0500

    Final commitment of files. Out of town for the weekend with no way to edit.

---
+++ Weekly Topic Unit 3/GeometricShapes_Tests/GeometricShapes_Tests/Square_Tests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GeometricShapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace GeometricShapes_Tests
+{
+    [TestClass]
+    public class Square_Tests
+    {
+        [TestMethod]
+        public void Verify_That_Number_Of_Sides_Is_3()
+        {
+            Square square = new Square();
+
+            square.NumberOfSides.ShouldBe(4);
+        }
+
+        [TestMethod]
+        public void Verify_The_Side_Length_May_Be_Set_And_Retrieved()
+        {
+            Square square = new Square();
+            double expectedValue = 3.14d;
+
+            square.SideLength = expectedValue;
+
+            square.SideLength.ShouldBe(expectedValue);
+        }
+
+        [TestMethod]
+        public void Verify_The_Perimeter_May_Be_Set_And_Retrieved()
+        {
+            Square square = new Square();
+            double sideLength = 1.5d;
+            double expectedPerimeter = 6d;
+
+            square.SideLength = sideLength;
+
+            square.Perimeter().ShouldBe(expectedPerimeter);
+        }
+
+        [TestMethod]
+        public void Verify_That_Area_Is_Calculated_Accurately()
+        {
+            Square square = new Square();
+            double sideLength = 2d;
+            double minimumValuePossible = 3.9d;
+            double maximumValuePossible = 4.1d;
+
+            square.SideLength = sideLength;
+
+            square.Area().ShouldBeInRange(minimumValuePossible, maximumValuePossible);
+        }
+
+        [TestMethod]
+        public void Verify_Total_Sum_Of_Angles_Can_Be_Retrieved()
+        {
+            Square square = new Square();
+
+            square.TotalMeasureOfAllAngles().ShouldBe(180);
+
+            
+        }
+    }
+}
