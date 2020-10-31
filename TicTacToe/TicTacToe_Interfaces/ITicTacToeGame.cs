8199523957f52fcebf8e1d7f5ded2b474127a604
Mark Reynolds <Mark@ProfReynolds.com>
2020-10-31 06:37:20 -0500

    ProfReynolds fixes per the assignment comments

--- TicTacToe/TicTacToe_Interfaces/ITicTacToeGame.cs
+++ TicTacToe/TicTacToe_Interfaces/ITicTacToeGame.cs
@@ -6,10 +6,33 @@ namespace TicTacToe_Interfaces
 {
     public interface ITicTacToeGame
     {
+        /// <summary>
+        /// used to store the name of the player
+        /// </summary>
         string PlayerName { get; set; }
 
+        void ResetGrid();
+
+        /// <summary>
+        /// Identifies the cell owner of a cell in a specific row - col
+        /// </summary>
+        /// <param name="CellRow">the row of the cell</param>
+        /// <param name="CellCol">the column opf the cell</param>
+        /// <returns>the identity of the owner of the cell</returns>
         CellOwners IdentifyCellOwner(int CellRow, int CellCol);
 
-        void SetCellOwner(int CellRow, int CellCol, CellOwners CellOwner);
+        /// <summary>
+        /// sets the cell owner of a cell in a specific row - col
+        /// </summary>
+        /// <param name="CellRow">the row of the cell</param>
+        /// <param name="CellCol">the column opf the cell</param>
+        /// <param name="CellOwner">the new owner of the cell</param>
+        void AssignCellOwner(int CellRow, int CellCol, CellOwners CellOwner);
+
+        void AutoPlayComputer();
+
+        bool CheckForWinner();
+
+        string IdentifyWinner();
     }
 }
